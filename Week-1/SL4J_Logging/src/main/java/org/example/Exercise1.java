c08d856aa770afe66e7a118b4262ef2618aa9fb0
sinhabytes <azmuthsinha101@gmail.com>
2026-07-08 02:25:08 +0530

    Add files via upload

---
+++ Week-1/SL4J_Logging/src/main/java/org/example/Exercise1.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Exercise1 {
+
+    private static final Logger logger =
+            LoggerFactory.getLogger(Exercise1.class);
+
+    public static void main(String[] args) {
+
+        logger.error("This is an error message");
+
+        logger.warn("This is a warning message");
+
+    }
+}
